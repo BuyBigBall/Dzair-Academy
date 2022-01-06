9a780e6dcbaa6c3e0df8ec7ca2239352cb2a2e5f
Autur Nasonov <86613656+BuyBigBall@users.noreply.github.com>
2022-01-07 08:57:40 +0900

    iussue

--- app/helpers.php
+++ app/helpers.php
@@ -22,6 +22,15 @@ if( !defined('ARCHIVE_EXTENSIONS')) define('ARCHIVE_EXTENSIONS', [
 if( !defined('MAX_COURSE_UPLOAD_SIZE')) define('MAX_COURSE_UPLOAD_SIZE', 1024);
 //We use this to convert date to new format
 
+function seo_friendly_url($string){
+    $string = str_replace(array('[\', \']'), '', $string);
+    $string = preg_replace('/\[.*\]/U', '', $string);
+    $string = preg_replace('/&(amp;)?#?[a-z0-9]+;/i', '-', $string);
+    $string = htmlentities($string, ENT_COMPAT, 'utf-8');
+    $string = preg_replace('/&([a-z])(acute|uml|circ|grave|ring|cedil|slash|tilde|caron|lig|quot|rsquo);/i', '\\1', $string );
+    $string = preg_replace(array('/[^a-z0-9]/i', '/[-]+/') , '-', $string);
+    return strtolower(trim($string, '-'));
+}
 
 function agotime($date)
 {
